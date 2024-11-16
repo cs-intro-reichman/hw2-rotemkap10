090fa4844c60d2d57db8b0f68f5e81102b389dde
Rotem Kaplan <RotemKaplan@Rotem-Kaplans-Mackbook.local>
2024-11-16 14:30:13 +0200

    complited 5 exrecises test 1

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,26 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	   String name = args[0];
+           int numberOfTimes = Integer.parseInt(args[1]);
+           for(int i = 0; i<name.length(); i++)
+           {
+                if(name.charAt(i) == 'A' || name.charAt(i) == 'E' || name.charAt(i) == 'f'|| name.charAt(i) == 'H' || name.charAt(i) == 'I' || name.charAt(i) == 'L'||
+                name.charAt(i) == 'M' || name.charAt(i) == 'N'|| name.charAt(i) == 'O'|| name.charAt(i) == 'R' || name.charAt(i) == 'S' || name.charAt(i) == 'X')
+                {
+                        System.out.println("Give me an" + " "  + name.charAt(i) + ":" + " " + name.charAt(i) + "!");
+                }
+                else
+                {
+                        System.out.println("Give me a" + " " + " " + name.charAt(i) + ":" + " " + name.charAt(i) + "!");  
+                }
+           }
+           System.out.println("What does that spell?");
+           for(int i =0; i<numberOfTimes; i++)
+           {
+                System.out.println(args[0] + "! ! !");
+           }
+
+
         }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,79 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+	    int n = Integer.parseInt(args[0]);
+		int d = n;
+		String mode1 = "v";
+		String mode2 = "c";
+		String mode = args[1];
+		if(mode2.equals(mode))
+				{
+					System.out.println("Every one of the first " + n + " hailstone sequences reached 1");
+					return;
+				}
+		if(d ==1)
+		{
+			System.out.println("1 4 2 1 (4)");
+			System.out.println("Every one of the first 1 hailstone sequences reached 1.");
+			return;
+		}
+		else
+		{
+			
+			System.out.println("1 4 2 1 (4)");
+			for(int i =2; i<n+1; i++)
+			{
+				
+				int k = i;
+				int numberOfTimes = 1;
+				System.out.print(k + " ");
+				if(mode2.equals(mode))
+				{
+
+				}
+				while(k!=1)
+				{
+					if(k%2==0)
+					{
+						k = k/2;
+					}
+					else
+					{
+						k = (k*3) + 1;
+					}
+					if(k!=1)
+					{
+						System.out.print(k + " ");
+					}
+					else
+					{
+						System.out.print(k);
+					}
+					numberOfTimes++;
+				
+			}
+			System.out.println( " " + "(" +  +numberOfTimes + ")");
+		}
+		}
+		System.out.println("Every one of the first" + " " + n + " " + "hailstone sequences reached 1.");
+		
+		
 }
+}
+
+		
+		
+
+	
+		
+			
+			
+			
+			
+		
+		
+		
+		
+		
+	
+
