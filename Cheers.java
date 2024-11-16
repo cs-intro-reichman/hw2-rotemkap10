924b24e4fc5d200a7b554019403753b0ce3b25ba
Rotem Kaplan <RotemKaplan@Rotem-Kaplans-Mackbook.local>
2024-11-16 15:40:34 +0200

    fixed the last bug

--- Cheers.java
+++ Cheers.java
@@ -7,7 +7,7 @@ public class Cheers {
            for(int i = 0; i<name.length(); i++)
            {
                 if(upperCase.charAt(i) == 'A' || upperCase.charAt(i) == 'E' || upperCase.charAt(i) == 'f'|| name.charAt(i) == 'H' || upperCase.charAt(i) == 'I' || upperCase.charAt(i) == 'L'||
-                upperCase.charAt(i) == 'M' || upperCase.charAt(i) == 'N'|| upperCase.charAt(i) == 'O'|| upperCase.charAt(i) == 'R' || upperCase.charAt(i) == 'S' || upperCase.charAt(i) == 'X')
+                upperCase.charAt(i) == 'M' || upperCase.charAt(i) == 'N'|| upperCase.charAt(i) == 'O'|| upperCase.charAt(i) == 'R' || upperCase.charAt(i) == 'S' || upperCase.charAt(i) == 'X'|| upperCase.charAt(i) == 'F')
                 {
                         System.out.println("Give me an" + " "  + upperCase.charAt(i) + ":" + " " + upperCase.charAt(i) + "!");
                 }
